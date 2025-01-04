54076f530b412ee9abb8fb8ad1b2215513613cec
Sansan1980 <152459976+Sansan1980@users.noreply.github.com>
2025-01-04 22:41:26 +0300

    Initial commit

---
+++ src/main/java/com/skypro/greeter050125/Greeter050125Application.java
@@ -0,0 +1,13 @@
+package com.skypro.greeter050125;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Greeter050125Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Greeter050125Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/greeter050125/Greeter050125ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.greeter050125;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Greeter050125ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
