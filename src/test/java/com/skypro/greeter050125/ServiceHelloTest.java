3659a12fb9d99349019c1b794cd8526edf260a71
Sansan1980 <152459976+Sansan1980@users.noreply.github.com>
2025-01-05 23:41:41 +0300

    Два конструктора в ServiseHello + моки на randomHello ServiceHello

--- src/test/java/com/skypro/greeter050125/ServiceHelloTest.java
+++ src/test/java/com/skypro/greeter050125/ServiceHelloTest.java
@@ -2,10 +2,14 @@ package com.skypro.greeter050125;
 
 import com.skypro.greeter050125.service.ServiceHello;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.Random;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ServiceHelloTest {
     private ServiceHello serviceHello;
@@ -20,4 +24,42 @@ public class ServiceHelloTest {
         serviceHello = new ServiceHello(randomMock);// передаем мок объект Random в сервис public class ServiceHello
     }
 
+    @Test
+    @DisplayName("Мок  метод randomHello когда пришла еденица и case  1-> Hello ")
+    public void whenRandomIsOne_ThenReturnHello() {
+        //программируем мок возвращать еденицу
+        when(randomMock.nextInt(1, 5)).thenReturn(1);
+        String result = serviceHello.randomHello();
+        //Проверяем результат метода randomHello с заданным в этом методе  значением по case ( 1-> "Hello")
+        assertEquals("Hello", result);
+    }
+    @Test
+    @DisplayName("Мок  метод randomHello когда пришла 2 и case  2-> Privet ")
+    public void whenRandomIsTwo_ThenReturnPrivet() {
+        //программируем мок возвращать еденицу
+        when(randomMock.nextInt(1, 5)).thenReturn(2);
+        String result = serviceHello.randomHello();
+        //Проверяем результат метода randomHello с заданным в этом методе  значением по case ( 2-> "Hello")
+        assertEquals("Privet", result);
+
+    }
+    @Test
+    @DisplayName("Мок  метод randomHello когда пришла 3 и case  3-> Salyt ")
+    public void whenRandomIsThree_ThenReturnSalyt() {
+        //программируем мок возвращать еденицу
+        when(randomMock.nextInt(1, 5)).thenReturn(3);
+        String result = serviceHello.randomHello();
+        //Проверяем результат метода randomHello с заданным в этом методе  значением по case ( 3-> "Hello")
+        assertEquals("Salyt", result);
+    }
+    @Test
+    @DisplayName("Мок  метод randomHello когда пришла 4 и case  4-> Salyt ")
+    public void whenRandomIsFourOrMore_ThenReturnSalyt() {
+        //программируем мок возвращать еденицу
+        when(randomMock.nextInt(1, 5)).thenReturn(4);
+        String result = serviceHello.randomHello();
+        //Проверяем результат метода randomHello с заданным в этом методе  значением по case ( 4-> "Hay")
+        assertEquals("Hay", result);
+
+    }
 }
